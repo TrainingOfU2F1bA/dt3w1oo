3e702f9df5312c120837ebd075d8184763811ecf
saul <eternx@163.com>
2018-04-08 18:13:49 +0800

    <008> refactor(practice9.*):pass testcase of practice9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,54 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    int number;
+    private Student leader;
+
+    public Klass(int num) {
+        this.number = num;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+
+    public boolean equals(Klass obj) {
+       return number ==obj.number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student jerry) {
+        if (appendMember(jerry)) {
+            leader =jerry;
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public boolean appendMember(Student stu) {
+        if (stu.getKlass().equals(this)) {
+            stu.setKlass(this);
+        }else {
+            System.out.print("It is not one of us.\n");
+            return false;
+        }
+        return true;
+    }
 }
