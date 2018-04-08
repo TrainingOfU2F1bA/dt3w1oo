86711ddd1bf1bf8c91315f01675810d6e97abfd2
saul <eternx@163.com>
2018-04-08 16:08:04 +0800

    <005> refactor(practice6.*):pass testcase of practice6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,27 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Teacher. I teach "+(klass==null?"No Class.":("Class "+klass+"."));
+    }
 }
