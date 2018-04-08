f1d46aaab45ae0fd0c5dc28b00736c476e972712
saul <eternx@163.com>
2018-04-08 21:57:49 +0800

    <010> refactor(practice11):pass testcases of practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,65 @@
 package practice11;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Klass {
+    int number;
+    private Student leader;
+    private List<Teacher> teachers=new LinkedList<>();
+
+    public Klass(int num) {
+        this.number = num;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Klass) {
+            if (((Klass)obj).getNumber()==number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student) {
+        if (this.equals(student.getKlass())) {
+            leader =student;
+            teachers.stream().forEach(teacher->teacher.awareofLeaderChange(this,student));
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public boolean appendMember(Student stu) {
+        teachers.stream().forEach(teacher->teacher.awareofMemberChange(this,stu));
+        stu.setKlass(this);
+        return true;
+    }
+
+    public void addTecher(Teacher teacher){
+        teachers.add(teacher);
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,52 @@
 package practice11;
 
-public class Teacher {
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private HashSet<Klass> classes;
+
+    public Teacher(int id, String name, int age, HashSet<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        classes.stream().forEach(s->s.addTecher(this));
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        boolean b = classes == null || classes.isEmpty();
+        return super.introduce() + " I am a Teacher. I teach " +
+                (b ?
+                        "No Class." : ("Class "+ (classes!=null&&!classes.isEmpty()?classes.stream().
+                        mapToInt(x->x.getNumber()).sorted().boxed().collect(Collectors.toList()).toString().replaceAll("\\[|\\]",""):"")
+                                + "."));
+    }
+
+    public String introduceWith(Student student) {
+
+        return super.introduce() + " I am a Teacher. I " + (!classes.contains(student.getKlass()) ? "don't " : "") + "teach " + student.getName() + ".";
+    }
+
+    public HashSet<Klass> getClasses() {
+        return classes;
+    }
+
+    public boolean isTeaching(Student jerry) {
+        return classes.contains(jerry.getKlass());
+    }
+
+    public void awareofLeaderChange(Klass klass,Student student) {
+        System.out.print("I am "+getName()+". I know "+student.getName()+" become Leader of "+klass.getDisplayName()+".\n");
+    }
+    public void awareofMemberChange(Klass klass,Student student) {
+        System.out.print("I am "+getName()+". I know "+student.getName()+" has joined "+klass.getDisplayName()+".\n");
+    }
 }
