941caa8ab1bc4ced07d89a9264936f190e470528
saul <eternx@163.com>
2018-04-08 16:47:51 +0800

    <007> refactor(practice8.*):pass testcase of practice8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,39 @@
 package practice08;
 
 public class Klass {
+    int number;
+    private Student leader;
+
+    public Klass(int num) {
+        this.number = num;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+
+    public boolean equals(Klass obj) {
+       return number ==obj.number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student jerry) {
+        leader =jerry;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,41 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Teacher. I teach "+(klass==null?"No Class.":("Class "+klass+"."));
+    }
+
+    public String introduceWith(Student student) {
+
+        return super.introduce()+" I am a Teacher. I "+(!klass.equals(student.getKlass())?"don't ":"")+"teach "+student.getName()+".";
+    }
 }
