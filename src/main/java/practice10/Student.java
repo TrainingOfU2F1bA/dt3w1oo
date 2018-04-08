737116e5349f238b899e7244237e9a0a4adab165
saul <eternx@163.com>
2018-04-08 18:53:42 +0800

    <009> refactor(practice10.*):pass testcase of practice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,28 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am "+(klass!=null&&this.equals(klass.getLeader())?"Leader of":"at")+" Class "+klass+".";
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,44 @@
 package practice10;
 
-public class Teacher {
+import com.google.common.collect.Multimap;
+
+import java.util.HashSet;
+
+public class Teacher extends Person {
+    private HashSet<Klass> classes;
+
+    public Teacher(int id, String name, int age, HashSet<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        boolean b = classes == null || classes.isEmpty();
+        return super.introduce() + " I am a Teacher. I teach " +
+                (b ?
+                        "No Class." : ("Class "+ (classes!=null&&!classes.isEmpty()?classes.toString().replaceAll("\\[|\\]", ""):"")
+                                + "."));
+    }
+
+    public String introduceWith(Student student) {
+
+        return super.introduce() + " I am a Teacher. I " + (!classes.contains(student.getKlass()) ? "don't " : "") + "teach " + student.getName() + ".";
+    }
+
+    public HashSet<Klass> getClasses() {
+        return classes;
+    }
+
+    public boolean isTeaching(Student jerry) {
+        return classes.contains(jerry.getKlass());
+    }
 }
