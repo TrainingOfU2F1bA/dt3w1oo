206bb6215969d5d05a4050154c6d7232270d1e02
saul <eternx@163.com>
2018-04-08 16:28:04 +0800

    <006> refactor(practice7.*):pass testcase of practice7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,30 @@
 package practice07;
 
 public class Klass {
+    int number;
+
+    public Klass(int num) {
+        this.number = num;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+
+    public boolean equals(Klass obj) {
+       return number ==obj.number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,32 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Teacher. I teach "+(klass==null?"No Class.":("Class "+klass+"."));
+    }
+
+    public String introduceWith(Student student) {
+
+        return super.introduce()+" I am a Teacher. I "+(!klass.equals(student.getKlass())?"don't ":"")+"teach "+student.getName()+".";
+    }
 }
