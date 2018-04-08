0b67a180dfbbaca33f4b3596656daa9d2922d55a
saul <eternx@163.com>
2018-04-08 15:47:26 +0800

    <003> refactor(practice[234].*):pass testcase of practice4

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,7 +1,31 @@
 package practice03;
 
-public class Person extends practice02.Person{
+public class Person {
+    private String name;
+    private int age;
+
     public Person(String name, int age) {
-        super(name, age);
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is "+name+". I am "+age+" years old.";
     }
 }
